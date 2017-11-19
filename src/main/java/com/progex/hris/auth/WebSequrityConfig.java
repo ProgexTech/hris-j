2e7b9cbb69aaa6ec148f3b5e0b09e38bf9aff43c
imoremada <imoremada@gmail.com>
2017-11-19 12:57:05 +0530

    Password encode and Spring Security related basic stuff

---
+++ src/main/java/com/progex/hris/auth/WebSequrityConfig.java
@@ -0,0 +1,15 @@
+package com.progex.hris.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSequrityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http.csrf().disable();
+	  }
+}
--- src/main/java/com/progex/hris/user/UserServiceImpl.java
+++ src/main/java/com/progex/hris/user/UserServiceImpl.java
@@ -5,39 +5,41 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.progex.hris.user.*;
 
 @Component
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
 	
-	
+
 	public List<User> getAllUsers() {
 		List<User> users = new ArrayList<User>();
 		userRepository.findAll().forEach(users::add);
 		return users;
 	}
-	
+
 	public User getUser(long id) {
 		return userRepository.findOne(id);
 	}
-	
+
 	public void addUser(User user) {
+		user.setpWord(new BCryptPasswordEncoder().encode(user.getpWord()));
 		userRepository.save(user);
 	}
-	
+
 	public void updateUser(long id, User user) {
 		userRepository.save(user);
 	}
-	
+
 	public void deleteUser(long id) {
 		userRepository.delete(id);
 	}
-	
+
 	public User getUserByUserName(String uName) {
 		return userRepository.findByUserName(uName);
 	}
