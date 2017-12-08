5cd08c4931f27e7b52f097551f703d6f8ff4e376
imoremada <imoremada@gmail.com>
2017-12-08 14:38:55 +0530

    Cross origin support for whole application

--- src/main/java/com/progex/hris/HrisApplication.java
+++ src/main/java/com/progex/hris/HrisApplication.java
@@ -3,6 +3,10 @@ package com.progex.hris;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EntityScan("com.progex.hris")
@@ -11,4 +15,18 @@ public class HrisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrisApplication.class, args);
 	}
+	
+	  @Bean
+	    public WebMvcConfigurerAdapter corsConfigurer() {
+	        return new WebMvcConfigurerAdapter() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	            	registry.addMapping("/api/**")
+	        		.allowedOrigins("http://localhost:4200")
+	        		.allowedMethods("PUT", "DELETE", "GET", "PATCH")
+	        		.allowedHeaders("*")
+	        		.allowCredentials(false).maxAge(3600);
+	            }
+	        };
+	    }
 }
