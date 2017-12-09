59ee170023f6593c642f0e1ba564ab3e2090c8a2
shanchathuranga <shanchathuranga@gmail.com>
2017-12-09 07:32:59 +0530

    logs added to LeaveTypeController class

--- src/main/java/com/progex/hris/leave/leaveType/LeaveTypeController.java
+++ src/main/java/com/progex/hris/leave/leaveType/LeaveTypeController.java
@@ -2,7 +2,11 @@ package com.progex.hris.leave.leaveType;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +17,45 @@ public class LeaveTypeController {
 	
 	@Autowired
 	private LeaveTypeService leaveService;
+	private static final Logger logger = LoggerFactory.getLogger(LeaveTypeController.class);
 
 	@RequestMapping("/leaveTypes")
-	public List<LeaveType> getAllLeaveTypes() {
-		return leaveService.getAllLeaveTypes();
+	public ResponseEntity<List<LeaveType>> getAllLeaveTypes() {
+		if (logger.isDebugEnabled()) {
+			logger.debug("LeaveTypeController::getAllLeaveTypes() called");
+		}
+		List<LeaveType> leaveTypeList = leaveService.getAllLeaveTypes();
+		if (leaveTypeList.isEmpty()) {
+			if (logger.isInfoEnabled()) {
+				logger.info("LeaveTypeController::getAllLeaveTypes() empty result returned");
+			}
+			
+			return new ResponseEntity<List<LeaveType>>(HttpStatus.NO_CONTENT);
+		}
+		if (logger.isInfoEnabled()) {
+			logger.info("LeaveTypeController::getAllLeaveTypes() non-empty result returned");
+		}
+		
+		return new ResponseEntity<List<LeaveType>>(leaveTypeList, HttpStatus.OK);
 	}
 	
 	@RequestMapping("/leaveTypes/{code}")
-	public LeaveType getLeaveType(@PathVariable String code) {
-		return leaveService.getLeaveType(code);
+	public ResponseEntity<LeaveType> getLeaveType(@PathVariable String code) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("LeaveTypeController::getLeaveType() called");
+		}
+		LeaveType leaveType = leaveService.getLeaveType(code);
+		if (leaveType == null) {
+			if (logger.isInfoEnabled()) {
+				logger.info("LeaveTypeController::getLeaveType() null result returned");
+			}
+			
+			return new ResponseEntity<LeaveType>(HttpStatus.NO_CONTENT);
+		}
+		if (logger.isInfoEnabled()) {
+			logger.info("LeaveTypeController::getLeaveType() non-null result returned");
+		}
+		
+		return new ResponseEntity<LeaveType>(leaveType, HttpStatus.OK);
 	}
 }
